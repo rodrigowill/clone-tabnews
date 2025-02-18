8765e9067b1f4c5d2ed8c407c27548eec2092f0b
Rodrigo dos Santos Soares <37874230+rodrigowill@users.noreply.github.com>
2025-02-18 23:19:19 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>A pressa é inimiga da perfeição. A perfeição é Deus. Então, a pressa é inimiga de Deus!</h1>
+    return <h1>Helena, I love you!</h1>
 }
 
 export default Home;
